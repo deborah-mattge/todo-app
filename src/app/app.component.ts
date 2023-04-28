2fd18ed22b736360356bf3352c1061a5db064003
deborah-mattge <elomattge@gmail.com>
2023-04-27 22:00:18 -0300

    foio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
+interface Pessoa{
+  nome:string
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'todo-app';
+  mostraInput: boolean=true;
+  usuarios: Pessoa[]=[];
+    pessoa: Pessoa ={
+      nome:''
+
+    }
+    cadastrarUsuario():void{
+      
+      
+
+      const usuario: Pessoa={
+        nome:this.pessoa.nome
+      }
+
+      
+      this.usuarios.push(usuario);
+      this.salvar();
+      this.pessoa.nome=''
+      console.log(this.usuarios);
+    }
+    remover(indice:number){
+      this.usuarios.splice(indice,1);
+      this.salvar()
+    }
+    salvar(){
+      localStorage.setItem('tarefa',JSON.stringify(this.usuarios));
+
+    }
+    
+    
+  
+    
+  
 }
